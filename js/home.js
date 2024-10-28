daf572dcb13a057a7c840c92b7da7dd349eb86c4
rifayethasan11 <rifayet005@gmail.com>
2024-10-28 16:01:36 +0600

    handle add money event handler on the button click

--- js/home.js
+++ js/home.js
@@ -1,1 +1,18 @@
-console.log('home added');
+// add money to the account
+// S-1: add event handler
+// prevent page reload after form submit
+// S-2: get money to be added to the account balance
+
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    // S-2: get money to be added to the account balance
+    const addMonetInput = document.getElementById('input-add-money').value;
+    console.log(addMonetInput);
+
+    // get the pin number provided
+    const pinNumber = document.getElementById('input-pin-number').value;
+    console.log(pinNumber);
+
+})
