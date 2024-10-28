7f1538c31cb0a098409d6f994ca8e77f3245455f
rifayethasan11 <rifayet005@gmail.com>
2024-10-28 13:51:53 +0600

    handle login and redirect to the home page

--- js/login.js
+++ js/login.js
@@ -9,5 +9,17 @@ document.getElementById('button-login').addEventListener('click', function(event
 
     // step-3: get the phone number and the pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value
+
+    console.log(phoneNumber, pinNumber);
+    // step-4: validate phone and Pin
+    // this is temporary. You should do like this.
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href = "./home.html"
+    }
+    else{
+        alert('Somenthing is wrong please check again.')
+    }
+    
 })
